33554dda4069ca6414d37d73ced032694f5d1b2b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-03 11:48:35 +0000

    Added getNumberInfo at script.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Function to Get Number Information (including Invalid Input)
+function getNumberInfo() {
+    // Set Up Variable
+    let txt = "";
+    // Get the Value of the Input Field
+    let num = document.getElementById('mynumber').value;
+    // Check if Number is Valid
+    if (isNaN(num) || num.toString().length == 0 || num<1000 || num>9999 || !Number.isInteger(Number(num))) {
+        txt += `Invalid Input.  Please enter the year between 1000 and 9999 as a whole number.`;
+    } else {
+        // Get Number Information
+        txt += `You have entered the Year ${num}. </p>`;
+        // Check if the Number is a Leap Year
+        if (isLeapYear(num)) {
+            txt += `${num} is a Leap Year.`;
+        } else {
+            txt += `${num} is not a Leap Year.`;
+        }
+    }
+
+    // Display Information in the Browser Window
+    document.getElementById('numinfo').innerHTML = txt;
+}
